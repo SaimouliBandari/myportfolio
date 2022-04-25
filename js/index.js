1372a6851af4d40d7873b218fd1e081a28fcda7d
SaimouliBandari <saimouli4k@gmail.com>
2022-04-25 18:07:04 +0530

    Under maintenance

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
-var txt = "An aspiring and visonary individual currently pursuing his graduation " +
+var txt = "Mouli is an aspiring and visonary individual currently pursuing his graduation " +
 "in Electronic and Communication at Raghu Engineering College from city " +
 "of Desitiny - Visakhapatnam.He is a fast learner and software developer "+
 "enthusiast and loves building project.";
 
 var i =0;
 var speed = 60;
+var finish = false;
 function typeWriter() {
     if (i < txt.length) {
         document.getElementById("write").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
+        if(i === (txt.length-1)){
+            alert("This site is under Maintenance");
+        }
     }
+    
 }
 $(document).ready(function(){
     $("div.outer_avatar").hover(function(){
@@ -23,11 +28,16 @@ $(document).ready(function(){
         $(".ifram_display").remove();
         $(".text_display").add();
        $(this).delay(2000).queue(function(){
-        typeWriter();
+            typeWriter();
        });
+       
     },9000);
 
     
+
+   
+
+   
 
    
     
