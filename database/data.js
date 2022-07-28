e9c81c027f69e2f66a2d440857ba290806217958
SaimouliBandari <saimouli4k@gmail.com>
2022-07-29 00:23:04 +0530

    added list and database to page

---
+++ database/data.js
@@ -0,0 +1,19 @@
+export const DATA = [
+    
+    {
+         "id" : 0,
+         "Name" : "Java Training",
+         "src" : "images/certificates/Core Java Training - Certificate of Completion.jpg"
+     },
+     {
+         "id" : 1,
+         "Name" : "HTML CSS and Js",
+         "src" : "images/certificates/HTML CSS Duke.jpg"
+     },
+     {
+         "id" : 2,
+         "Name" : "Intern",
+         "src" : "images/certificates/beyonders intern.jpg"
+     }
+
+]
--- js/certificates.js
+++ js/certificates.js
@@ -1,23 +1,59 @@
+import {DATA} from "../database/data.js";
 var domWidth = $(document).width();
+var gridView, listView, icon = $(".grid-icon").attr("src") ;
+var renderOnce = true;
 $(document).ready(function () {
     if(domWidth <= 425){
         $(".certificatesDiv").addClass("order-first");
     }
     $(".viewButton").click(
         
+        
         function(){
-            var gridView = "images/grid-icon-12.jpg";
-            var listView = "images/list.png";
-            var icon = $(".grid-icon").attr("src");
-
-            if(icon === listView){
-                $(".grid-icon").attr("src" , "images/grid-icon-12.jpg");
+            
+            gridView = "images/grid-icon-12.jpg";
+            listView = "images/list.png";
+            icon = $(".grid-icon").attr("src");
+            
+            if(icon === gridView){
+                $(".grid-icon").attr("src" , listView);
+                $("table.t_view").removeClass("d-none");
+                if(renderOnce){
+                    listdisp();
+                    renderOnce = false;
+                }
             }else{
-                $(".grid-icon").attr("src" , "images/list.png");
+                $(".grid-icon").attr("src" ,gridView);
+                $("table.t_view").addClass("d-none");
+                griddisp();
             }
+
         }
        
     );
-   
+    
+    function listdisp() {
+        // console.log("List View");
+
+        for (let i = 0; i < DATA.length; i++) {
+           
+            $(".t_view_body").append(`<tr id="R${i}">
+            <td >
+            ${DATA[i].id + 1}
+            </td>
+            <td>
+              ${DATA[i].Name} Certificate
+            </td>
+            <td>
+            </td>
+             </tr>`).css("color","white");
+            // console.log(new Image(DATA[i].src).sizes);
+        };
+
+    };
+
+    function griddisp() {
+        console.log("Grid View");
+    };
 
 });
